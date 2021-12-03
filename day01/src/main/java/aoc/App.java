a3c9607bd4bbc3c193d1cd65874e68ae381f8823
Pär Tjärnberg <par.tjarnberg@cygni.se>
2021-12-03 17:56:56 +0100

    Refactor

--- day01/src/main/java/aoc/App.java
+++ day01/src/main/java/aoc/App.java
@@ -7,23 +7,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class App {
-    private final List<Integer> input;
-
-    public App(List<Integer> input) {
-        this.input = input;
+    public Integer getSolutionPart1(final List<Integer> input) { // 1387
+        final int[] increased = { 0 };
+        input.stream().reduce((a, b) -> {
+            if(a != 0 && b > a)
+                increased[0]++;
+            return b;
+        });
+        return increased[0];
     }
 
-    public Integer getSolutionPart1() { // 1387
-        int prev = input.get(0), increased = 0;
-        for(int i = 1; i < input.size(); i++) {
-            if(input.get(i) > prev)
-                increased++;
-            prev = input.get(i);
-        }
-        return increased;
-    }
-
-    public Integer getSolutionPart2() { // 1362
+    public Integer getSolutionPart2(final List<Integer> input) { // 1362
         int prev = input.get(0) + input.get(1) + input.get(2), increased = 0;
         for(int i = 2; i < input.size(); i++) {
             int currentWindow = input.get(i - 2) + input.get(i - 1) + input.get(i);
@@ -35,18 +29,14 @@ public class App {
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println("java");
-        List<Integer> input = parseInput("input.txt");
-        String part = System.getenv("part") == null ? "part1" : System.getenv("part");
+        final String part = System.getenv("part") == null ? "part1" : System.getenv("part");
         if (part.equals("part2"))
-            System.out.println(new App(input).getSolutionPart2());
+            System.out.println(new App().getSolutionPart2(parseInput()));
         else
-            System.out.println(new App(input).getSolutionPart1());
+            System.out.println(new App().getSolutionPart1(parseInput()));
     }
 
-    private static List<Integer> parseInput(String filename) throws IOException {
-        return Files.lines(Path.of(filename))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+    private static List<Integer> parseInput() throws IOException {
+        return Files.lines(Path.of("input.txt")).map(Integer::parseInt).collect(Collectors.toList());
     }
 }
