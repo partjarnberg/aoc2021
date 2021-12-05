e2a518c004b5dd80a4d35dc5853b0dd845cdafa4
Pär Tjärnberg <par.tjarnberg@cygni.se>
2021-12-05 08:42:34 +0100

    Cleanup

--- day04/src/main/java/aoc/App.java
+++ day04/src/main/java/aoc/App.java
@@ -1,3 +1,5 @@
+package aoc;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,7 +8,6 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static java.lang.Integer.parseInt;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
@@ -15,18 +16,17 @@ import static java.util.stream.IntStream.range;
 public class App {
     private static class Cell {
         final int number; boolean marked;
-        private Cell(int number, final boolean marked) {
+
+        Cell(int number) {
             this.number = number;
-            this.marked = marked;
+            this.marked = false;
         }
     }
 
     private static class Board {
-        final Cell[][] cells;
-        int winningNumber = -1;
+        final Cell[][] cells; int winningNumber = -1;
 
-        public Board(final Cell[][] cells) {
-            checkArgument(cells.length == 5);
+        Board(final Cell[][] cells) {
             this.cells = cells;
         }
 
@@ -35,27 +35,26 @@ public class App {
                     stream(cells[row]).filter(cell -> number == cell.number)
                             .forEach(cell -> {
                                 cell.marked = true;
-                                if(hasWinningRow() || hasWinningCol()) {
+                                if(hasAWinningRow() || hasAWinningCol()) {
                                     winningNumber = number;
-                                }
-                            }));
+                                }}));
         }
 
-        private boolean hasWinningRow() {
-            return range(0, 5).anyMatch(row -> stream(cells[row]).allMatch(cell -> cell.marked));
-        }
-
-        private boolean hasWinningCol() {
-            return range(0, 5).anyMatch(col -> range(0, 5).allMatch(row -> cells[row][col].marked)
-            );
-        }
-
-        public boolean isAWinner() {
+        boolean isAWinner() {
             return winningNumber != -1;
         }
 
-        public int sumOfUnmarked() {
+        int sumOfUnmarked() {
             return range(0, 5).map(row -> range(0, 5).filter(col -> !cells[row][col].marked).map(col -> cells[row][col].number).sum()).sum();
+        }
+
+        private boolean hasAWinningRow() {
+            return range(0, 5).anyMatch(row -> stream(cells[row]).allMatch(cell -> cell.marked));
+        }
+
+        private boolean hasAWinningCol() {
+            return range(0, 5).anyMatch(col -> range(0, 5).allMatch(row -> cells[row][col].marked)
+            );
         }
     }
 
@@ -65,7 +64,7 @@ public class App {
         return winner.winningNumber * winner.sumOfUnmarked();
     }
 
-    public Integer getSolutionPart2(final List<Integer> randoms, List<Board> boards) { // 13912
+    public Integer getSolutionPart2(final List<Integer> randoms, final List<Board> boards) { // 13912
         randoms.forEach(number -> boards.forEach(board -> board.mark(number)));
         final Board winner = boards.stream().filter(Board::isAWinner).max(Comparator.comparingInt(board -> randoms.indexOf(board.winningNumber))).orElseThrow();
         return winner.winningNumber * winner.sumOfUnmarked();
@@ -80,11 +79,11 @@ public class App {
                     final String[] s = boardString.replace("  ", " ").split(" ");
                     final Cell[][] cells = new Cell[5][5];
                     final AtomicInteger i = new AtomicInteger();
-                    range(0, 5).forEach(row -> range(0, 5).forEach(col -> cells[row][col] = new Cell(parseInt(s[i.getAndIncrement()]), false)));
+                    range(0, 5).forEach(row -> range(0, 5).forEach(col -> cells[row][col] = new Cell(parseInt(s[i.getAndIncrement()]))));
                     return new Board(cells);
                 }).collect(toList());
         final String part = System.getenv("part") == null ? "part1" : System.getenv("part");
-        if (part.equals("part1"))
+        if (part.equals("part2"))
             System.out.println(new App().getSolutionPart2(randoms, boards));
         else
             System.out.println(new App().getSolutionPart1(randoms, boards));
