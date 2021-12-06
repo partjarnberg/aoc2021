ea954f73e28048c39e614eda33fb8db1e5793eb2
Pär Tjärnberg <par.tjarnberg@cygni.se>
2021-12-06 10:24:18 +0100

    Make it better

--- day06/src/main/java/App.java
+++ day06/src/main/java/App.java
@@ -1,26 +1,22 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import static java.util.Arrays.stream;
-import static java.util.stream.IntStream.range;
 import static java.util.stream.IntStream.rangeClosed;
 
 public class App {
-    public long simulate(final long[] fishTimers, final int numberOfDays) {
-        range(0, numberOfDays).forEach(day -> {
-            var spawn = fishTimers[0];
-            rangeClosed(0, 7).forEach(timer -> fishTimers[timer] = fishTimers[timer + 1]);
-            fishTimers[6] += spawn;
-            fishTimers[8] = spawn;
+    public long simulate(final long[] lanternFishTimers, final int numberOfDays) {
+        rangeClosed(1, numberOfDays).forEach(day -> {
+            var spawn = lanternFishTimers[0];
+            rangeClosed(0, 7).forEach(timer -> lanternFishTimers[timer] = lanternFishTimers[timer + 1]);
+            lanternFishTimers[6] += spawn; lanternFishTimers[8] = spawn;
         });
-        return stream(fishTimers).sum();
+        return stream(lanternFishTimers).sum();
     }
 
     public static void main(String[] args) throws IOException {
-        final long[] fishTimers = new long[9];
-        stream(Files.readString(Path.of("input.txt")).split(",")).map(Integer::parseInt).forEach(timer -> fishTimers[timer]++);
-        final String part = System.getenv("part") == null ? "part1" : System.getenv("part");
-        System.out.println(new App().simulate(fishTimers, part.equalsIgnoreCase("part1") ? 80 : 256));
+        final long[] lanternFishTimers = new long[9];
+        stream(Files.readString(Path.of("input.txt")).split(",")).map(Integer::parseInt).forEach(timer -> lanternFishTimers[timer]++);
+        System.out.println(new App().simulate(lanternFishTimers, (System.getenv("part") == null ? "part1" : System.getenv("part")).equalsIgnoreCase("part1") ? 80 : 256));
     }
 }
