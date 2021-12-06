e478fdef502093a622072cd63a344b5a2f0c0ff7
Pär Tjärnberg <par.tjarnberg@cygni.se>
2021-12-06 10:43:49 +0100

    Better naming

--- day06/src/main/java/App.java
+++ day06/src/main/java/App.java
@@ -5,18 +5,18 @@ import static java.util.Arrays.stream;
 import static java.util.stream.IntStream.rangeClosed;
 
 public class App {
-    public long simulate(final long[] lanternFishTimers, final int numberOfDays) {
+    public long simulate(final long[] lanternfishLifecycle, final int numberOfDays) {
         rangeClosed(1, numberOfDays).forEach(day -> {
-            var spawn = lanternFishTimers[0]; var newParents = lanternFishTimers[0];
-            rangeClosed(0, 7).forEach(timer -> lanternFishTimers[timer] = lanternFishTimers[timer + 1]);
-            lanternFishTimers[8] = spawn; lanternFishTimers[6] += newParents;
+            var spawn = lanternfishLifecycle[0]; var justBecameParents = lanternfishLifecycle[0];
+            rangeClosed(0, 7).forEach(stage -> lanternfishLifecycle[stage] = lanternfishLifecycle[stage + 1]);
+            lanternfishLifecycle[8] = spawn; lanternfishLifecycle[6] += justBecameParents;
         });
-        return stream(lanternFishTimers).sum();
+        return stream(lanternfishLifecycle).sum();
     }
 
     public static void main(String[] args) throws IOException {
-        final long[] lanternFishTimers = new long[9];
-        stream(Files.readString(Path.of("input.txt")).split(",")).map(Integer::parseInt).forEach(timer -> lanternFishTimers[timer]++);
-        System.out.println(new App().simulate(lanternFishTimers, (System.getenv("part") == null ? "part1" : System.getenv("part")).equalsIgnoreCase("part1") ? 80 : 256));
+        final long[] lanternfishLifecycle = new long[9];
+        stream(Files.readString(Path.of("input.txt")).split(",")).map(Integer::parseInt).forEach(stage -> lanternfishLifecycle[stage]++);
+        System.out.println(new App().simulate(lanternfishLifecycle, (System.getenv("part") == null ? "part1" : System.getenv("part")).equalsIgnoreCase("part1") ? 80 : 256));
     }
 }
