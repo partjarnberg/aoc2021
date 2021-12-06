c0ac20018a0f20f9352ec6db6596d37d0e658e98
Pär Tjärnberg <par.tjarnberg@cygni.se>
2021-12-06 13:51:19 +0100

    Using Collections rotate

--- day06/src/main/java/aoc/App.java
+++ day06/src/main/java/aoc/App.java
@@ -1,24 +1,19 @@
 package aoc;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import static java.util.Arrays.stream;
-import static java.util.stream.IntStream.rangeClosed;
+import java.io.IOException; import java.nio.file.*; import java.util.*;
+import static java.util.Arrays.stream; import static java.util.Collections.rotate; import static java.util.stream.IntStream.rangeClosed;
 
 public class App {
-    public long simulate(final long[] lanternfishLifecycle, final int numberOfDays) {
+    public long simulate(final List<Long> lanternfishLifecycle, final int numberOfDays) {
         rangeClosed(1, numberOfDays).forEach(day -> {
-            var spawn = lanternfishLifecycle[0]; var justBecameParents = lanternfishLifecycle[0];
-            rangeClosed(0, 7).forEach(stage -> lanternfishLifecycle[stage] = lanternfishLifecycle[stage + 1]);
-            lanternfishLifecycle[8] = spawn; lanternfishLifecycle[6] += justBecameParents;
+            rotate(lanternfishLifecycle, -1);
+            lanternfishLifecycle.set(6, lanternfishLifecycle.get(6) + lanternfishLifecycle.get(8));
         });
-        return stream(lanternfishLifecycle).sum();
+        return lanternfishLifecycle.stream().reduce(Long::sum).orElseThrow();
     }
 
     public static void main(String[] args) throws IOException {
-        final long[] lanternfishLifecycle = new long[9];
-        stream(Files.readString(Path.of("input.txt")).split(",")).map(Integer::parseInt).forEach(stage -> lanternfishLifecycle[stage]++);
+        final List<Long> lanternfishLifecycle = new ArrayList<>(stream(new long[9]).boxed().toList());
+        stream(Files.readString(Path.of("input.txt")).split(",")).map(Integer::parseInt).forEach(stage -> lanternfishLifecycle.set(stage, lanternfishLifecycle.get(stage) + 1 ));
         System.out.println(new App().simulate(lanternfishLifecycle, (System.getenv("part") == null ? "part1" : System.getenv("part")).equalsIgnoreCase("part1") ? 80 : 256));
     }
 }
