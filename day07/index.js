1b8d1040b22baf41ab8581182dab698775c7c310
Pär Tjärnberg <par.tjarnberg@cygni.se>
2021-12-07 09:41:19 +0100

    Simplify

--- day07/index.js
+++ day07/index.js
@@ -1,37 +1,27 @@
 const fs = require('fs')
 
-function horizontalPositions(filename="input.txt") {
-    return fs.readFileSync(filename).toString().trim().split(",").map((x)=>parseInt(x))
-}
-
 function costToAlign(goal, positions) {
-    return positions.map(currentPosition => Math.abs(currentPosition - goal)).reduce((a, b) => a + b, 0)
+    return positions.map(currPos => Math.abs(currPos - goal)).reduce((a, b) => a + b, 0)
 }
 
-function getSolutionPart1() { // 329389
-    const positions = horizontalPositions();
+function getSolutionPart1(positions) { // 329389
     return Math.min(...[...Array(Math.max(...positions) + 1).keys()].map(goal => costToAlign(goal, positions)));
 }
 
 function costToAlignWithVariableRate(goal, positions) {
-    return  positions.map(position => {
-        let distance = Math.abs(position - goal);
+    return  positions.map(currPos => {
+        let distance = Math.abs(currPos - goal);
         return distance * (1 +  distance) / 2;
     }).reduce((a, b) => a + b, 0);
 }
 
-function getSolutionPart2() { // 86397080
-    const positions = horizontalPositions();
+function getSolutionPart2(positions) { // 86397080
     return Math.min(...[...Array(Math.max(...positions) + 1).keys()].map(goal => costToAlignWithVariableRate(goal, positions)));
 }
 
-const part = process.env.part || "part2"
-
+const part = process.env.part || "part1";
+let positions = fs.readFileSync("input.txt").toString().trim().split(",").map(x => parseInt(x));
 if (part === "part1")
-    console.log(getSolutionPart1())
+    console.log(getSolutionPart1(positions))
 else
-    console.log(getSolutionPart2())
-
-module.exports = {
-    getSolutionPart1, getSolutionPart2, inputDataLinesIntegers: horizontalPositions
-}
+    console.log(getSolutionPart2(positions))
