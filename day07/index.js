c495cf8915b94b984531bdcba8cb99651c56df74
Pär Tjärnberg <par.tjarnberg@cygni.se>
2021-12-07 09:52:58 +0100

    Refactor

--- day07/index.js
+++ day07/index.js
@@ -1,25 +1,23 @@
 const fs = require('fs')
 
-function costToAlign(goal, positions) {
-    return positions.map(currPos => Math.abs(currPos - goal)).reduce((a, b) => a + b, 0)
+function costToAlign(goal, positions, costFunction) {
+    return positions.map(currPos => costFunction(currPos)).reduce((a, b) => a + b, 0)
 }
 
 function getSolutionPart1(positions) { // 329389
-    return Math.min(...[...Array(Math.max(...positions) + 1).keys()].map(goal => costToAlign(goal, positions)));
-}
-
-function costToAlignWithVariableRate(goal, positions) {
-    return  positions.map(currPos => {
-        let distance = Math.abs(currPos - goal);
-        return distance * (1 +  distance) / 2;
-    }).reduce((a, b) => a + b, 0);
+    return Math.min(...[...Array(Math.max(...positions) + 1).keys()].map(goal => costToAlign(goal, positions, function (currPos) {
+        return Math.abs(currPos - goal);
+    })));
 }
 
 function getSolutionPart2(positions) { // 86397080
-    return Math.min(...[...Array(Math.max(...positions) + 1).keys()].map(goal => costToAlignWithVariableRate(goal, positions)));
+    return Math.min(...[...Array(Math.max(...positions) + 1).keys()].map(goal => costToAlign(goal, positions, function (currPos) {
+        let distance = Math.abs(currPos - goal);
+        return distance * (1 +  distance) / 2;
+    })));
 }
 
-const part = process.env.part || "part1";
+const part = process.env.part || "part2";
 let positions = fs.readFileSync("input.txt").toString().trim().split(",").map(x => parseInt(x));
 if (part === "part1")
     console.log(getSolutionPart1(positions))
