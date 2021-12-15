9e9ab237bccd9bd488e315e65b0f1bb96bfe264e
Pär Tjärnberg <par.tjarnberg@cygni.se>
2021-12-15 09:43:52 +0100

    Part 2

--- day14/src/main/java/App.java
+++ day14/src/main/java/App.java
@@ -1,67 +1,68 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 import static java.lang.System.getenv;
-import static java.util.Comparator.comparingInt;
 import static java.util.Comparator.comparingLong;
-import static java.util.stream.Collectors.*;
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.IntStream.rangeClosed;
 
 public class App {
-    record PairInsertionRule(String pattern, Character insert) {}
-    record InsertInstruction(int index, Character insert) {}
+    record PairInsertionRule(String pair, Character insert, String firstNewPair, String secondNewPair) {}
 
     static class Polymer {
-        List<Character> polymerTemplate;
+        final Map<String, Long> polymerTemplate = new HashMap<>();
 
         Polymer(final String polymerTemplate) {
-            this.polymerTemplate = new LinkedList<>(polymerTemplate.chars().mapToObj(c -> (char) c).toList());
+            IntStream.range(0, polymerTemplate.length() - 1).forEach(index -> {
+                final String pair = polymerTemplate.substring(index, index + 2);
+                this.polymerTemplate.put(pair, this.polymerTemplate.getOrDefault(pair, 0L) + 1);
+            });
         }
 
         void applyRules(final List<PairInsertionRule> insertionRules) {
-            final String currentPolymer = polymerTemplate.stream().map(String::valueOf).collect(joining());
-            final AtomicInteger offset = new AtomicInteger();
-            insertionRules.stream().map(rule -> {
-                final List<Integer> indexes = new ArrayList<>();
-                int index = 0, indexOffset = 0;
-                while(index != -1){
-                    index = currentPolymer.indexOf(rule.pattern, index + indexOffset);
-                    if (index != -1) indexes.add(index);
-                    indexOffset = 1;
-                }
-                return indexes.stream().map(indexOf -> new InsertInstruction(indexOf + 1, rule.insert)).toList();
-            }).flatMap(List::stream).sorted(comparingInt(i -> i.index)).forEach(instruction ->
-                    polymerTemplate.add(instruction.index + offset.getAndIncrement(), instruction.insert)
-            );
+            final Map<String, Long> newPolymerTemplate = new HashMap<>();
+            insertionRules.forEach(rule -> {
+                final long count = polymerTemplate.getOrDefault(rule.pair, 0L);
+                newPolymerTemplate.put(rule.firstNewPair, newPolymerTemplate.getOrDefault(rule.firstNewPair, 0L) + count);
+                newPolymerTemplate.put(rule.secondNewPair, newPolymerTemplate.getOrDefault(rule.secondNewPair, 0L) + count);
+                newPolymerTemplate.put(rule.pair, newPolymerTemplate.getOrDefault(rule.pair, 0L) - count);
+            });
+            newPolymerTemplate.forEach((key, value) -> {
+                long previousValue = ofNullable(polymerTemplate.get(key)).orElse(0L);
+                if(previousValue + value <= 0)
+                    polymerTemplate.remove(key);
+                else polymerTemplate.put(key, previousValue + value);
+            });
         }
+
 
         Map<Character, Long> getOccurances() {
-            return polymerTemplate.stream().collect(groupingBy(c -> c, counting()));
-        }
-
-        public String toString() {
-            return polymerTemplate.stream().map(String::valueOf).collect(joining());
+            final Map<Character, Long> charCounts = new HashMap<>();
+            polymerTemplate.forEach((key, value) -> {
+                if(charCounts.containsKey(key.charAt(1)))
+                    charCounts.put(key.charAt(1), charCounts.get(key.charAt(1)) + value);
+                else
+                    charCounts.put(key.charAt(1), value);
+            });
+            return charCounts;
         }
     }
 
     public long solvePart1(final Polymer polymer, final List<PairInsertionRule> insertionRules) { // 2891
-        IntStream.rangeClosed(1, 10).forEach(step -> polymer.applyRules(insertionRules));
+        rangeClosed(1, 10).forEach(step -> polymer.applyRules(insertionRules));
         final Map.Entry<Character, Long> max = polymer.getOccurances().entrySet().stream().max(comparingLong(Map.Entry::getValue)).orElseThrow();
         final Map.Entry<Character, Long> min = polymer.getOccurances().entrySet().stream().min(comparingLong(Map.Entry::getValue)).orElseThrow();
         return max.getValue() - min.getValue();
     }
 
-    public long solvePart2(final Polymer polymer, final List<PairInsertionRule> insertionRules) {
-        IntStream.rangeClosed(1, 40).forEach(step -> {
-            System.out.println("Step " + step);
-            polymer.applyRules(insertionRules);
-        });
+    public long solvePart2(final Polymer polymer, final List<PairInsertionRule> insertionRules) { // 4607749009683
+        rangeClosed(1, 40).forEach(step -> polymer.applyRules(insertionRules));
         final Map.Entry<Character, Long> max = polymer.getOccurances().entrySet().stream().max(comparingLong(Map.Entry::getValue)).orElseThrow();
         final Map.Entry<Character, Long> min = polymer.getOccurances().entrySet().stream().min(comparingLong(Map.Entry::getValue)).orElseThrow();
         return max.getValue() - min.getValue();
@@ -71,9 +72,10 @@ public class App {
         final Polymer polymer = new Polymer(Files.lines(Path.of("input.txt")).limit(1).collect(joining()));
         final List<PairInsertionRule> insertionRules = Files.lines(Path.of("input.txt")).skip(2).map(line -> {
             final String[] split = line.split(" -> ");
-            return new PairInsertionRule(split[0], split[1].charAt(0));
+            final String pair = split[0]; final Character insertElement = split[1].charAt(0);
+            return new PairInsertionRule(pair, insertElement, "" + pair.charAt(0) + insertElement, "" + insertElement + pair.charAt(1));
         }).toList();
-        System.out.println((getenv("part") == null ? "part2" : getenv("part")).equalsIgnoreCase("part1") ?
+        System.out.println((getenv("part") == null ? "part1" : getenv("part")).equalsIgnoreCase("part1") ?
                 new App().solvePart1(polymer, insertionRules) :
                 new App().solvePart2(polymer, insertionRules));
     }
